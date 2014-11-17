f41f01c767a76ce520ac97573dc432eb05fc61d3
Andreas <ciscoheat@gmail.com>
2014-11-17 11:23:39 +0100

    Completed the API

--- src/mithril/M.hx
+++ src/mithril/M.hx
@@ -1,5 +1,8 @@
 package mithril;
+import js.Browser;
+import js.html.DOMWindow;
 import js.html.Element;
+import js.Error;
 
 abstract Either<T1, T2>(Dynamic) from T1 from T2 to T1 to T2 {}
 
@@ -16,6 +19,25 @@ typedef VirtualElement = {
 	var tag : String;
 	var attributes : Dynamic;
 	var children : Dynamic;
+};
+
+typedef Promise<T> = {
+	function then(?success : Void -> T, ?error : Void -> T) : Promise<T>;
+}
+
+typedef Deferred<T> = {
+	var promise : Promise<T>;
+	function resolve(value : T) : Void;
+	function reject(value : T) : Void;
+}
+
+typedef XHROptions = Dynamic;
+
+typedef JSONPOptions = {
+	var dataType : String;
+	var url : String;
+	var callbackKey : String;
+	var data : Dynamic;
 };
 
 class M
@@ -76,5 +98,60 @@ class M
 	}
 	public static function set_routeMode(s : String) {
 		return untyped __js__("Mithril.route.mode = s");
+	}
+
+	public static function request<T>(options : Either<XHROptions, JSONPOptions>) : Promise<T>
+	{
+		return untyped __js__("Mithril.request(options)");
+	}
+
+	public static function deferred<T>() : Deferred<T>
+	{
+		return untyped __js__("Mithril.deferred()");
+		// TODO: function onerror(e : Error) : Void;
+	}
+
+	public static function sync<T>(promises : Array<Promise<T>>) : Promise<T>
+	{
+		return untyped __js__("Mithril.sync(promises)");
+	}
+
+	public static function trust(html : String) : String
+	{
+		return untyped __js__("Mithril.trust(html)");
+	}
+
+	public static function render(rootElement : Element, children : Dynamic, forceRecreation : Bool)
+	{
+		return untyped __js__("Mithril.render(rootElement, children, forceRecreation)");
+	}
+
+	public static function redraw(forceSync : Bool) : Void
+	{
+		return untyped __js__("Mithril.redraw(forceSync)");
+	}
+
+	public static var redrawStrategy(get, set) : String;
+
+	public static function get_redrawStrategy() {
+		return untyped __js__("Mithril.redraw.strategy");
+	}
+	public static function set_redrawStrategy(s : String) {
+		return untyped __js__("Mithril.redraw.strategy = s");
+	}
+
+	public static function startComputation() : Void
+	{
+		return untyped __js__("Mithril.startComputation()");
+	}
+
+	public static function endComputation() : Void
+	{
+		return untyped __js__("Mithril.endComputation()");
+	}
+
+	public static function deps(window : Dynamic) : DOMWindow
+	{
+		return untyped __js__("Mithril.deps(window)");
 	}
 }
