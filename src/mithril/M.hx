17a85286f38238717defe85eaf47ce9e753d9bd7
Andreas <ciscoheat@gmail.com>
2014-11-22 16:30:14 +0100

    Server-side rendering ported.

--- src/mithril/M.hx
+++ src/mithril/M.hx
@@ -1,5 +1,6 @@
 package mithril;
 
+#if js
 import js.Browser;
 import js.html.Document;
 import js.html.DOMWindow;
@@ -7,8 +8,10 @@ import js.html.Element;
 import js.Error;
 import js.html.Event;
 import js.html.XMLHttpRequest;
+#end
 
 using Lambda;
+using StringTools;
 
 private abstract Either<T1, T2, T3, T4>(Dynamic)
 from T1 from T2 from T3 from T4 to T1 to T2 to T3 to T4 {}
@@ -42,6 +45,10 @@ typedef MithrilModule<T> = {
 	function view(ctrl : T) : ViewOutput;
 }
 
+#if !js
+private typedef Event = Dynamic;
+#end
+
 typedef GetterSetter<T> = ?T -> T;
 typedef EventHandler<T : Event> = T -> Void;
 
@@ -67,6 +74,7 @@ typedef Deferred<T, T2> = {
 	function reject(value : T2) : Void;
 }
 
+#if js
 /**
  * Plenty of optional fields for this one:
  * http://lhorie.github.io/mithril/mithril.request.html#signature
@@ -94,9 +102,11 @@ typedef JSONPOptions = {
 	@:optional var callbackKey : String;
 	@:optional var data : Dynamic;
 };
+#end
 
 //////////
 
+#if js
 @:final @:native("Mithril")
 extern class M
 {
@@ -183,3 +193,141 @@ extern class M
 	// Stores the current module so it can be used in module() calls (added automatically by macro).
 	@:noCompletion public static var __cm : Dynamic;
 }
+#else
+class M
+{
+	static var parser : EReg = ~/(?:(^|#|\.)([^#\.\[\]]+))|(\[.+?\])/g;
+	static var attrParser : EReg = ~/\[(.+?)(?:=("|'|)(.*?)\2)?\]/;
+	static var voidElements : EReg = ~/^(AREA|BASE|BR|COL|COMMAND|EMBED|HR|IMG|INPUT|KEYGEN|LINK|META|PARAM|SOURCE|TRACK|WBR)$/;
+	static var voidElementsArray = ['area', 'base', 'br', 'col', 'command', 'embed', 'hr', 
+									'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr'];
+
+
+	public static function m(selector : String, ?attributes : Dynamic, ?children : Children) : VirtualElement {
+		var hasAttrs = Reflect.isObject(attributes) && Type.getClass(attributes) == null &&
+			!Reflect.hasField(attributes, "tag") && !Reflect.hasField(attributes, "subtree");
+
+		var attrs = hasAttrs ? attributes : {};
+		var classAttrName = Reflect.hasField(attributes, "class") ? "class" : "className";
+		var cell : Dynamic = {tag: "div", attrs: {}};
+		var match = [];
+		var classes = [];
+
+		var isEmpty = function(s : String) { return s == null || s == ""; };
+
+		var tempSelector = selector;
+		while(parser.match(tempSelector)) {
+			var m = parser.matched;
+			if(m(1) == "" && !isEmpty(m(2))) cell.tag = m(2);
+			else if(m(1) == "#") cell.attrs.id = m(2);
+			else if(m(1) == ".") classes.push(m(2));
+			else {
+				var attr = m(3);
+				if(attr.substr(0, 1) == "[") {
+					attrParser.match(attr);
+					var value = attrParser.matched(3);
+					Reflect.setField(cell.attrs, attrParser.matched(1), 
+						isEmpty(value) 
+							? isEmpty(attrParser.matched(2)) ? "" : true
+							: value
+					);
+				}
+			}
+			tempSelector = parser.matchedRight();
+		}
+		if(classes.length > 0) Reflect.setField(cell, classAttrName, classes.join(" "));
+
+		var realChildren : Children = cast (children == null ? attributes : children);
+		if(Std.is(realChildren, Array)) {
+			Reflect.setField(cell, "children", realChildren);
+		} else {
+			var childArray = [];
+			if(attributes != null && !hasAttrs) childArray.push(attributes);
+			else if(children != null) childArray.push(children);
+			Reflect.setField(cell, "children", childArray);
+		}
+
+		//trace('##### $selector #####');
+
+		for (attrName in Reflect.fields(attrs)) {
+			var attrValue = Reflect.field(attrs, attrName);			
+			//trace('# $attrName => ' + attrValue);
+			if(attrName == classAttrName) {
+				var cellValue = Reflect.field(cell.attrs, attrName);
+				Reflect.setField(cell.attrs, attrName, (cellValue == null ? "" : cellValue) + " " + attrValue);
+			} else {
+				Reflect.setField(cell.attrs, attrName, attrValue);
+			}
+		}
+
+		return cell;
+	}
+
+	public static function render(view : ViewOutput) : String {
+		if(view == null) return "";
+		if(Std.is(view, String)) return cast view;
+		if(Std.is(view, Array)) return cast(view, Array<Dynamic>).map(render).join('');
+
+		// view must be a VirtualElement now.
+		var el : VirtualElement = cast view;
+		if(Reflect.hasField(el, "$trusted")) return createTrustedContent(el);
+
+		var children = createChildrenContent(el);
+		if(children.length == 0 && voidElementsArray.indexOf(el.tag) >= 0) {
+			return '<' + el.tag + createAttrString(el.attrs) + '>';
+		}
+
+		return [
+			'<', el.tag, createAttrString(el.attrs), '>',
+			children,
+			'</', el.tag, '>'
+		].join('');
+	}
+
+	static function createChildrenContent(el : VirtualElement) : String {
+		if(el.children == null || !Std.is(el.children, Array)) return '';
+		return render(cast el.children);
+	}
+
+	static function createTrustedContent(el : VirtualElement) : String {
+		return null;
+	}
+
+	static function createAttrString(attrs : Dynamic) {
+		if(attrs == null || Reflect.fields(attrs).length == 0) return '';
+
+		return Reflect.fields(attrs).map(function(name) {
+			var value = Reflect.field(attrs, name);
+			if(Reflect.isFunction(value)) return '';
+
+			if(name == 'style') {
+				return ' style="' + Reflect.fields(value).map(function(property) {
+					return [camelToDash(property).toLowerCase(), Reflect.field(value, property)].join(':');
+				}).join(';') + '"';
+			}
+			return ' ' + (name == 'className' ? 'class' : name) + '="' + value + '"';
+		}).join('');
+	}
+
+	static function camelToDash(str : String) {
+		str = (~/\W+/g).replace(str, '-');
+		return (~/([a-z\d])([A-Z])/g).replace(str, '$1-$2');		
+	}
+
+	///// Stubs /////
+
+	public static function startComputation() : Void {}
+	public static function endComputation() : Void {}
+	public static function withAttr<T, T2>(property : String, ?callback : T -> Void) : EventHandler<T2> { return function(_) {}; }
+	public static function deferred<T, T2>() : Deferred<T, T2> { 
+		return {
+			promise: null,
+			resolve: function(_) {},
+			reject: function(_) {}
+		}; 
+	}
+
+	// Stores the current module so it can be used in module() calls (added automatically by macro).
+	@:noCompletion public static var __cm : Dynamic;
+}
+#end
