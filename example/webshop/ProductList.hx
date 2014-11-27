cd0918dbe581eb5781130ec2e29c14e8f992734a
Andreas <ciscoheat@gmail.com>
2014-11-27 02:15:39 +0100

    ProductList now using M.prop()

--- example/webshop/ProductList.hx
+++ example/webshop/ProductList.hx
@@ -9,58 +9,67 @@ using Lambda;
  */
 class ProductList implements Module<ProductList>
 {
-    var category : Category;
-    var products : Array<Product>;
+    @prop var category : Category;
+    @prop var products : Array<Product>;
+    @prop var loading : Bool;
 
-    public function new() {}
+    public function new() {
+        this.category = M.prop();
+        this.products = M.prop();
+        this.loading = M.prop();
+    }
 
     public function controller() {
-        // Create a temporary product with a loading text.
-        var loadingProduct = new Product();
-        loadingProduct.name = "Loading...";
-        loadingProduct.price = -1;
-
-        // Set products and category based on the current route.
-        products = [loadingProduct];
-        category = Category.get().find(function(c) 
+        products([]);
+        loading(true);
+        
+        // Set category based on the current route.
+        category(Category.get().find(function(c) 
             return c.id == M.routeParam("categoryId")
-        );
+        ));
 
         // Load products with an Ajax request.
-        Product.getByCategory(category.id).then(function(p) {
-            // Replace current products when ready
-            products.splice(0, products.length);
-            for(prod in p) products.push(prod);
-
-            // getByCategory is a background request to allow the
-            // menu to change, so redraw after it's finished.
+        // It has background = true to allow navigation to update,
+        // so a call to M.redraw is required when it finishes.
+        Product.getByCategory(category().id)
+        .then(products)
+        .then(function(_) {
+            loading(false);
             M.redraw();
         });
     }
 
     public function view() {
+        var content : VirtualElement;
+
+        if(loading()) {
+            content = m("h3", {style: {"text-align": "center"}}, "Loading...");
+        } else {
+            content = 
+                m(".table-responsive", [
+                    m("table.table.table-striped", [
+                        m("thead", [
+                            m("tr", [
+                                m("th", "#"),
+                                m("th", "Name"),
+                                m("th", "Price"),
+                                m("th", "Stock")
+                            ])
+                        ]),
+                        m("tbody#products", products().map(function(p) 
+                            return m("tr", [
+                                m("td", p.id),
+                                m("td", p.name),
+                                m("td", p.price >= 0 ? '$$${p.price}' : ""),
+                                m("td", {style: {color: p.stock < 10 ? "red" : ""}}, p.stock)
+                            ])
+                        ))
+                    ])
+                ]);
+        }
         return [
-            m("h2.sub-header", category.name),
-            m(".table-responsive", [
-                m("table.table.table-striped", [
-                    m("thead", [
-                        m("tr", [
-                            m("th", "#"),
-                            m("th", "Name"),
-                            m("th", "Price"),
-                            m("th", "Stock")
-                        ])
-                    ]),
-                    m("tbody#products", products.map(function(p) 
-                        return m("tr", [
-                            m("td", p.id),
-                            m("td", p.name),
-                            m("td", p.price >= 0 ? '$$${p.price}' : ""),
-                            m("td", {style: {color: p.stock < 10 ? "red" : ""}}, p.stock)
-                        ])
-                    ))
-                ])
-            ])
+            m("h2.sub-header", category().name),
+            content
         ];
     }
 }
